9f89204da5c8be4b61e6dc76ae4697bc9be5e84a
Hector Aurelio Medina Cornejo <49215127+HectorMedina124@users.noreply.github.com>
2020-11-17 22:03:10 -0600

    Update hill climb_8 reinas.py

--- hill climb_8 reinas.py
+++ hill climb_8 reinas.py
@@ -1,8 +1,6 @@
 import random
 from tkinter import ttk
 from tkinter import * 
-# función uno: el parámetro es el estado de diseño de tablero de ajedrez actual, 
-# y el logaritmo de reina de los conflictos de diseño de ocho reinas actuales se juzga de acuerdo con el diseño.
 def conflictos(posicion):
         num = 0
         for i in range(len(posicion)):
@@ -14,8 +12,7 @@ def conflictos(posicion):
                         if abs(posicion[i]-posicion[j]) == offset:
                                 num += 1
         return num
-#función dos: el parámetro es el estado actual del diseño del tablero de ajedrez, 
-#utilizando el método de escalada para seleccionar el mejor diseño del estado vecino y regresar
+
 def  hill_climbing(posicion):
         convert = {}
         length = len(posicion)
@@ -32,7 +29,7 @@ def  hill_climbing(posicion):
         conflict_now = conflictos(posicion) # logaritmo actual del conflicto de la reina
  
         
-        # Almacene todos los diccionarios sucesores posibles para encontrar el mejor sucesor
+        
         for aux1,aux2 in convert.items():
                 if aux2 < conflict_now:
                         conflict_now = aux2
@@ -40,7 +37,7 @@ def  hill_climbing(posicion):
                 if aux2 == conflict_now:
                         fitness.append(aux1)
  
-        #Si el mejor elemento del conjunto sucesor es mayor que uno, seleccione uno al azar
+        
         if len(fitness) > 0:
                 x = random.randint(0,len(fitness)-1)
                 columna = fitness[x][0]
@@ -48,12 +45,11 @@ def  hill_climbing(posicion):
                 posicion[columna] = fila
  
         return posicion
-# función tres: encuentre una solución para que las ocho reinas satisfagan el número de conflicto de 0 y 
-#recorra el conjunto posterior de cada paso hasta que no haya prisa> repentino 
+
 def reinas():
-        posicion = [0,1,2,3,4,5,6,7] # estado inicial, todas las reinas están en diagonal
+        posicion = [0,1,2,3,4,5,6,7] 
  
-        #Cuando el número de conflictos es mayor que 0, el bucle resuelve el mejor sucesor hasta encontrar la solución de ocho reinas.
+       
         while conflictos(posicion) > 0:
                 posicion = hill_climbing(posicion)
                 print (posicion)
